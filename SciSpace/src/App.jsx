9ddeba27ed7026dc0bdbe880c91aab03a405ea83
Mirzoyoqub Rashidov <97793909+mirzoyoqubcodes@users.noreply.github.com>
2023-11-11 11:01:51 +0500

    navbar added

--- SciSpace/src/App.jsx
+++ SciSpace/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Main/Main";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
